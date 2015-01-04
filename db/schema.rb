b1241259016edb14b8ba89ac9d8a4dc09ba1dca1
aaronrelph <aaron@imgonnarelph.com>
2015-01-03 17:06:40 -0800

    Gernerate Link Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150104010252) do
+
+  create_table "links", force: true do |t|
+    t.string   "title"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
